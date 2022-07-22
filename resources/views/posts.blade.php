7446a51fa782579c0d885aff64077962f542b977
sandro <sjr1999@protonmail.com>
2022-07-22 07:38:23 -0300

    Initial create

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="stylesheet" href="/app.css" />
+
+        <title>Laravel</title>
+
+    </head>
+    <body>
+        <?php foreach($posts as $post): ?>
+            <article>
+                <h1>
+                    <a href="/post/<?=$post->slug;?>"> 
+                        <?= $post->title; ?>
+                    </a>
+                </h1>
+
+                <p><?= $post->excerpt ;?></p>
+
+            </article>
+
+        <?php endforeach; ?>
+    </body>
+</html>
