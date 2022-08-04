b534410279c445c6fbbdb4c966577f37ef928317
Sandro Junior <sjr1999@protonmail.com>
2022-08-04 08:40:47 -0300

    Login

---
+++ app/Http/Controllers/SessionsController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+
+class SessionsController extends Controller
+{
+    public function create(){
+        return view('seesions:create');
+    }
+
+    public function store(){
+        $attributes = request()->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required']
+        ]);
+
+        $successLoggedIn = auth()->attempt($attributes);
+
+        if($successLoggedIn){
+            return redirect('/')->with(['success' => 'Welcome back!']);
+        }
+
+        return back()->withErrors(['email' => 'The provided credentials could not be verified.']);
+
+        // equivalente:
+        // throw ValidationException::withMessages([
+        //     'email' => 'The provided credentials could not be verified.'
+        // ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\SessionsController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PostController::class, 'index'])->name('home');
@@ -9,5 +10,7 @@ Route::get('/', [PostController::class, 'index'])->name('home');
 Route::get('/post/{post:slug}', [PostController::class, 'show']);
 
 Route::get('register', [RegisterController::class, 'create']);
-
 Route::post('register', [RegisterController::class, 'store']);
+
+Route::get('/login', [SessionsController::class, 'create']);
+Route::post('/login', [SessionsController::class, 'store']);
