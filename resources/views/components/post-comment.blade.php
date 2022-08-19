dec370a96d9759dd275c1e5bb61ebb720104e2ad
sandrojr <sjr1999@protonmail.com>
2022-08-19 17:27:55 -0300

    dynamic comments

--- resources/views/components/post-comment.blade.php
+++ resources/views/components/post-comment.blade.php
@@ -1,22 +1,25 @@
+
+@props(['comment'])
+
 <article class="flex bg-gray-100 border border-gray-200 p-6 rounded-xl space-x-4">
                     
     <div class="flex-shrink-0">
-        <img src="https://i.pravatar.cc/100" alt="" width="60" height="60" class="rounded-xl">
+        <img src="https://i.pravatar.cc/100?u={{ $comment->author->id }}" alt="" width="60" height="60" class="rounded-xl">
     </div>
 
     <div>
         <header class="mb-4">
-            <h3 class="font-bold">John Doe</h3>
+            <h3 class="font-bold">{{ $comment->author->name }}</h3>
 
             <p class="text-xs">
                 Posted
-                <time>8 months ago</time>
+                <time>{{ $comment->created_at }}</time>
             </p>
 
         </header>
 
         <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam deleniti labore laudantium? Accusantium excepturi ipsa sapiente suscipit eius veritatis nobis temporibus eaque ullam, minus voluptates aperiam iste non rerum illum.
+            {{ $comment->body }}
         </p>
        
     </div>
