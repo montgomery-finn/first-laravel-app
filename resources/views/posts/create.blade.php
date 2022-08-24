f193bd5334735a437746eded89044fcfcfc4f1f2
Sandro Junior <sjr1999@protonmail.com>
2022-08-24 15:52:11 -0300

    Dynamic posts thumbnails

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -2,8 +2,28 @@
     <section class="px-6 py-6">
         <x-panel class="max-w-sm mx-auto">
 
-            <form action="/admin/posts" method="POST">
+            <form action="/admin/posts" method="POST" enctype="multipart/form-data">
                 @csrf
+
+                <div class="mb-6">
+                    <label
+                        class="block mb-2 uppercase font-bold text-xs text-gray-700" 
+                        for="thumbnail">
+                            Thumbnail
+                    </label>
+
+                    <input 
+                        class="border border-gray-400 p-2 w-full"
+                        type="file"
+                        name="thumbnail"
+                        id="thumbnail"
+                        value="{{ old('thumbnail') }}"
+                        required>
+
+                    @error('thumbnail')
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
+                </div>
 
                 <div class="mb-6">
                     <label
